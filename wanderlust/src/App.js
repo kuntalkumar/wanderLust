3fa7f3b147950fba745101ff9682c5400b1f788e
kuntalkumar <kuntalkumar789@gmail.com>
2023-08-16 19:03:53 +0530

    Kuntal

--- wanderlust/src/App.js
+++ wanderlust/src/App.js
@@ -6,16 +6,22 @@ import Hotel from './Component/Hotels';
 
 // import Payment from './Component/Payment';
 import AllRoutes from './Routes/AllRoutes'
+import RazorpayPayment from './Component/Payment/RazorpayPayment';
 
 function App() {
   return (
     <div className="App">
       <AllRoutes/>
+<<<<<<< Updated upstream
     
+=======
+   {/* <h1>Hi i am ready</h1> */}
+>>>>>>> Stashed changes
       {/* <Payment/>
       <PlaceCard/> */}
       {/* <Hotel/> */}
       {/* <Details/> */}
+      {/* <RazorpayPayment/> */}
     </div>
   );
 }
